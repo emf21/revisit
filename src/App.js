a99eb855bfcfd3633565a0bb0adbd3858b9b1c28
emf21 <emefa.ameku@amalitech.org>
2024-03-30 22:05:18 +0000

    see code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello World
+          Hello World App
         </p>
         <a
           className="App-link"
